60ced2c2661e05816c7206c9a4287a6445ad8368
Ziyao Zhang <ziyaointl@gmail.com>
2016-10-13 12:08:02 +0800

    Basic working prototype

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-var quoteList = [];
+var lastIndex = -1;
+
+var quoteList = [
+	{
+		"quote": "Anyone who considers arithmetical methods of producing random digits is, of course, in a state of sin.",
+		"author": "-John von Neumann (1951)"
+	},
+	{
+		"quote": "Weeks of coding can save you hours of planning.",
+		"author": "-Anonymous"
+	}
+];
+
+function getQuote() {
+	var tempIndex = getRandomNumber(quoteList.length);
+	console.log(tempIndex);
+	while (lastIndex == tempIndex) {
+		var tempIndex = getRandomNumber(quoteList.length);
+		console.log(tempIndex);
+	}
+	lastIndex = tempIndex;
+	document.getElementById("quote").innerHTML = quoteList[tempIndex].quote;
+	document.getElementById("author").innerHTML = quoteList[tempIndex].author;
+}
+
+function getRandomNumber(max) {
+	return Math.floor(Math.random() * (max));
+}
