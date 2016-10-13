d43b5d3c0f3cdb5c5ec80a9ff1d8cd57aa41a7f1
Ziyao Zhang <ziyaointl@gmail.com>
2016-10-13 13:44:07 +0800

    Added more quotes

--- js/main.js
+++ js/main.js
@@ -3,11 +3,31 @@ var lastIndex = -1;
 var quoteList = [
 	{
 		"quote": "Anyone who considers arithmetical methods of producing random digits is, of course, in a state of sin.",
-		"author": "-John von Neumann (1951)"
+		"author": "-John von Neumann"
 	},
 	{
 		"quote": "Weeks of coding can save you hours of planning.",
 		"author": "-Anonymous"
+	},
+	{
+		"quote": "Should array indices start at 0 or 1? My compromise of 0.5 was rejected, without, I thought, proper consideration.",
+		"author": "-S. Kelly Bootle"
+	},
+	{
+		"quote": "Recursion n. See recursion.",
+		"author": "-Eric S. Raymond, The New Hacker's Dictionary"
+	},
+	{
+		"quote": "Computers are like bikinis. They save people a lot of guesswork.",
+		"author": "-Sam Ewin"
+	},
+	{
+		"quote": "That's what's cool about working with computers. They don't argue, they remember everything, and they don't drink all your beer.",
+		"author": "-Paul Leary"
+	},
+	{
+		"quote": "They have computers, and they may have other weapons of mass destruction.",
+		"author": "-Janet Reno"
 	}
 ];
 
